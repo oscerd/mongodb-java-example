b79d83508790cb97542741e575566285b3e53ad5
ancosen <ancosen@gmail.com>
2014-10-05 17:12:26 +0200

    Updated tests

--- src/test/java/com/ancosen/github/mongodb/MongodbTest.java
+++ src/test/java/com/ancosen/github/mongodb/MongodbTest.java
@@ -14,6 +14,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.WriteResult;
 
@@ -51,10 +52,41 @@ public class MongodbTest {
 	    	db.dropDatabase();
 	    	mongoClient.close();
 	}
-	
+		
 	@Test
 	public void collectionInsert() throws IOException{
-    	MongoClient mongoClient;
+    		MongoClient mongoClient;
+			MongoPropertyReader reader = new MongoPropertyReader("config.properties");
+			Properties properties = reader.getProperties();
+			org.junit.Assert.assertNotNull(properties);
+			MongoConfig config = new MongoConfig(properties.getProperty("mongodb.host"), Integer.parseInt(properties.getProperty("mongodb.port")));
+			mongoClient = new MongoClient( config.getMongoHost(), config.getMongoPort() );
+	    	DB db = mongoClient.getDB( "mydb" );
+	    	org.junit.Assert.assertNotNull(db);
+	    	
+	        BasicDBObject document = new BasicDBObject("author", "andrea")
+	        .append("text", "Pippo")
+	        .append("date", new Date());
+	        	    	
+	        DBCollection coll =  db.getCollection("pippo");
+	        
+	    	coll.insert(document);
+	    	
+	        Set<String> colls = db.getCollectionNames();
+	    	
+	    	org.junit.Assert.assertEquals(2, colls.size());
+	    	
+	    	DBCursor cursor = coll.find();
+	    	org.junit.Assert.assertEquals(1, cursor.size());
+	    	
+	    	coll.drop();
+	    	db.dropDatabase();
+	    	mongoClient.close();
+	}
+	
+	@Test
+	public void collectionFindOne() throws IOException{
+    		MongoClient mongoClient;
 			MongoPropertyReader reader = new MongoPropertyReader("config.properties");
 			Properties properties = reader.getProperties();
 			org.junit.Assert.assertNotNull(properties);
@@ -77,17 +109,55 @@ public class MongodbTest {
 	        	    	
 	        DBCollection coll =  db.getCollection("pippo");
 	        
-	    	WriteResult p = coll.insert(document);
+	    	coll.insert(document);
 	    	
-	    	ObjectId id = (ObjectId)document.get( "_id" );
+	    	coll.insert(document1);
 	    	
-	    	p = coll.insert(document1);
+	    	coll.insert(document2);
 	    	
-	    	id = (ObjectId)document.get( "_id" );
+	        Set<String> colls = db.getCollectionNames();
 	    	
-	    	p = coll.insert(document2);
+	    	org.junit.Assert.assertEquals(2, colls.size());
 	    	
-	    	id = (ObjectId)document.get( "_id" );
+	    	DBObject obj = coll.findOne();
+	    	org.junit.Assert.assertEquals(obj.get("author"), "andrea");
+	    	org.junit.Assert.assertEquals(obj.get("text"), "Pippo");
+	    	
+	    	coll.drop();
+	    	db.dropDatabase();
+	    	mongoClient.close();
+	}
+	
+	@Test
+	public void collectionFind() throws IOException{
+    		MongoClient mongoClient;
+			MongoPropertyReader reader = new MongoPropertyReader("config.properties");
+			Properties properties = reader.getProperties();
+			org.junit.Assert.assertNotNull(properties);
+			MongoConfig config = new MongoConfig(properties.getProperty("mongodb.host"), Integer.parseInt(properties.getProperty("mongodb.port")));
+			mongoClient = new MongoClient( config.getMongoHost(), config.getMongoPort() );
+	    	DB db = mongoClient.getDB( "mydb" );
+	    	org.junit.Assert.assertNotNull(db);
+	    	
+	        BasicDBObject document = new BasicDBObject("author", "andrea")
+	        .append("text", "Pippo")
+	        .append("date", new Date());
+	        
+	        BasicDBObject document1 = new BasicDBObject("author", "peppe")
+	        .append("text", "Pluto")
+	        .append("date", new Date());
+	        
+	        BasicDBObject document2 = new BasicDBObject("author", "joseph")
+	        .append("text", "Paperino")
+	        .append("date", new Date());
+	        	    	
+	        DBCollection coll =  db.getCollection("pippo");
+	        
+	        coll.insert(document);
+
+	    	coll.insert(document1);
+	    	
+	    	coll.insert(document2);
 	    	
 	        Set<String> colls = db.getCollectionNames();
 	    	
@@ -96,11 +166,54 @@ public class MongodbTest {
 	    	DBCursor cursor = coll.find();
 	    	org.junit.Assert.assertEquals(3, cursor.size());
 	    	
+	    	coll.drop();
+	    	db.dropDatabase();
+	    	mongoClient.close();
+	}
+	
+	@Test
+	public void collectionFindQuery() throws IOException{
+    		MongoClient mongoClient;
+			MongoPropertyReader reader = new MongoPropertyReader("config.properties");
+			Properties properties = reader.getProperties();
+			org.junit.Assert.assertNotNull(properties);
+			MongoConfig config = new MongoConfig(properties.getProperty("mongodb.host"), Integer.parseInt(properties.getProperty("mongodb.port")));
+			mongoClient = new MongoClient( config.getMongoHost(), config.getMongoPort() );
+	    	DB db = mongoClient.getDB( "mydb" );
+	    	org.junit.Assert.assertNotNull(db);
+	    	
+	        BasicDBObject document = new BasicDBObject("author", "andrea")
+	        .append("text", "Pippo")
+	        .append("date", new Date());
+	        
+	        BasicDBObject document1 = new BasicDBObject("author", "peppe")
+	        .append("text", "Pluto")
+	        .append("date", new Date());
+	        
+	        BasicDBObject document2 = new BasicDBObject("author", "joseph")
+	        .append("text", "Paperino")
+	        .append("date", new Date());
+	        	    	
+	        DBCollection coll =  db.getCollection("pippo");
+	        
+	    	coll.insert(document);
+	    	
+	    	coll.insert(document1);
+	    	
+	    	coll.insert(document2);
+	    	
+	        Set<String> colls = db.getCollectionNames();
+	    	
+	    	org.junit.Assert.assertEquals(2, colls.size());
+	    	
 	    	BasicDBObject query = new BasicDBObject("author", "peppe")
 	    	.append("text", "Pluto");
 	    	
-	    	cursor = coll.find(query);
+	    	DBCursor cursor = coll.find(query);
 	    	org.junit.Assert.assertEquals(1, cursor.size());
+	    	DBObject doc = cursor.next();
+	    	org.junit.Assert.assertEquals("peppe", doc.get("author"));
+	    	org.junit.Assert.assertEquals("Pluto", doc.get("text"));
 	    	
 	    	coll.drop();
 	    	db.dropDatabase();
